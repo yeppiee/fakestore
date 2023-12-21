6800fc65e7afd3f450b1ca136c3211514169804e
yeppiee <pakhotnykh.i@gmail.com>
2023-12-21 23:00:50 +0300

    init project

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import "@mantine/core/styles.css";
+import { Button, Center } from "@mantine/core";
+
+export const App = () => {
+  return (
+    <Center h="100%">
+      <Button variant="filled" color="green" radius="md">
+        Button
+      </Button>
+    </Center>
+  );
+};
