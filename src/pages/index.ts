9e739fc34749fa40273658d9e6d87e4a0f13862b
yeppiee <pakhotnykh.i@gmail.com>
2023-12-23 16:57:45 +0300

    add not found page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,7 +3,9 @@ import { createRoutesView } from 'atomic-router-react';
 import { HomeRoute } from '@/pages/home';
 import { SignInRoute } from '@/pages/signin';
 import { SignUpRoute } from '@/pages/signup';
+import { NotFound } from '@/pages/not-found/not-found';
 
 export const Routes = createRoutesView({
   routes: [HomeRoute, SignInRoute, SignUpRoute],
+  otherwise: NotFound,
 });
