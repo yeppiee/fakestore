bbc1456a1802173641e005c2548fbe71f154d26c
yeppiee <pakhotnykh.i@gmail.com>
2023-12-25 18:32:30 +0300

    add small navigate in not-found page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,3 +1,15 @@
+import { Center, Flex, Text } from '@mantine/core';
+import { Link } from 'atomic-router-react';
+
+import { routes } from '@/shared/config/routes';
+
 export const NotFound = () => {
-  return <div>not-found</div>;
+  return (
+    <Center h="100%">
+      <Flex direction="column" align="center" rowGap="xs">
+        <Text>This Page is Not Found</Text>
+        <Link to={routes.home}>Go to Home Page</Link>
+      </Flex>
+    </Center>
+  );
 };
